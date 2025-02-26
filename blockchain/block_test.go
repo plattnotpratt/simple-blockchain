d6e82cde35b87d0d57629e776c52a558de944877
w3cj <cj@null.computer>
2025-02-26 08:42:45 -0700

    Added unit test for length check of the hash

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -15,9 +15,10 @@ func TestCalculateHash(t *testing.T) {
 		Nonce:        3000,
 	}
 	checkHash := b.CalculateHash()
-	b.Index = 2
 	if checkHash != b.CalculateHash() {
-		t.Errorf("Hash not consistent and would always be invalid: got %s expected %s", checkHash[7:], b.CalculateHash()[7:])
+		t.Errorf("Hash not consistent and would always be invalid: got %s... expected %s...", checkHash[:7], b.CalculateHash()[:7])
 	}
-
+	if len(checkHash) != 64 {
+		t.Errorf("Hash is not the proper length: got %d and expected 64", len(checkHash))
+	}
 }
