b735ce19b1e13e27b8455adcc3d4197efa2ea50c
w3cj <cj@null.computer>
2025-03-01 10:50:27 -0700

    added test for create genesis block

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -22,3 +22,18 @@ func TestCalculateHash(t *testing.T) {
 		t.Errorf("Hash is not the proper length: got %d and expected 64", len(checkHash))
 	}
 }
+
+func TestCreateGenesisBlock(t *testing.T) {
+	gb := CreateGenesisBlock()
+
+	if gb.Index != 0 {
+		t.Errorf("Index which was created should be 0 instead it was %d", gb.Index)
+	}
+	if gb.PrevHash != "0" {
+		t.Errorf("Previous hash is set to something other than 0, ")
+	}
+
+	if len(gb.Hash) != 64 {
+		t.Errorf("Hash is not the proper length: got %d and expected 64", len(gb.Hash))
+	}
+}
